60c5eb7b3b12a74a21cd7714691c092eaec7172e
Naruesorn-N-Krabkhuntod <naruesorn.krabkhuntod@gmail.com>
2025-06-20 14:15:32 +0700

    fixed bug

--- server.js
+++ server.js
@@ -12,21 +12,6 @@ app.use(bodyParser.json());
 app.use(cors({origin: 'http://localhost:5500'}));
 
 
-const connection = mysql.createConnection({
-  host: '27.254.145.132',
-  user: 'zeniq_admin',
-  password: 'Zeniq12345!',
-  database: 'zeniq_payroll',
-  port: 3306
-});
-connection.connect((err) => {
-  if (err) {
-    console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
-    return;
-  }
-  console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
-});
-
 
 // API
 app.get('/', (req, res) => {
@@ -35,26 +20,74 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/allempoyee', (req, res) => {
+  const connection = mysql.createConnection({
+    host: '27.254.145.132',
+    user: 'zeniq_admin',
+    password: 'Zeniq12345!',
+    database: 'zeniq_payroll',
+    port: 3306
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
+      return;
+    }
+    console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
+  });
   connection.query('SELECT * FROM empoyee', (err, results) => {
     if (err) {
       return res.status(500).json({ error: 'เกิดข้อผิดพลาด' });
     }
     res.json(results);
   });
+  connection.end();
 });
 
+
+
 app.get('/api/allapprover', (req, res) => {
+  const connection = mysql.createConnection({
+    host: '27.254.145.132',
+    user: 'zeniq_admin',
+    password: 'Zeniq12345!',
+    database: 'zeniq_payroll',
+    port: 3306
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
+      return;
+    }
+    console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
+  });
   connection.query('SELECT * FROM approver', (err, results) => {
     if (err) {
       return res.status(500).json({ error: 'เกิดข้อผิดพลาด' });
     }
     res.json(results);
   });
+  connection.end();
 });
+
+
 
 app.get('/api/empoyee', (req, res) => {
   const { empoyee_id } = req.query;
   const sql = 'SELECT * FROM empoyee WHERE empoyee_id = ?';
+  const connection = mysql.createConnection({
+    host: '27.254.145.132',
+    user: 'zeniq_admin',
+    password: 'Zeniq12345!',
+    database: 'zeniq_payroll',
+    port: 3306
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
+      return;
+    }
+    console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
+  });
   connection.query(sql, [empoyee_id], (err, results) => {
     if (err) {
       return res.status(500).json({ error: 'เกิดข้อผิดพลาด' });
@@ -62,10 +95,25 @@ app.get('/api/empoyee', (req, res) => {
     console.log(results)
     res.json(results);
   });
+  connection.end();
 });
 
 
 app.get('/api/payroll', (req, res) => {
+  const connection = mysql.createConnection({
+    host: '27.254.145.132',
+    user: 'zeniq_admin',
+    password: 'Zeniq12345!',
+    database: 'zeniq_payroll',
+    port: 3306
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
+      return;
+    }
+    console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
+  });
   const { year, month } = req.query;
   const sql = `
     SELECT payroll.*, empoyee.*, approver.*
@@ -81,9 +129,24 @@ app.get('/api/payroll', (req, res) => {
     }
     res.json(results);  
   });
+  connection.end();
 });
 
 app.get('/api/payrollid', (req, res) => {
+  const connection = mysql.createConnection({
+    host: '27.254.145.132',
+    user: 'zeniq_admin',
+    password: 'Zeniq12345!',
+    database: 'zeniq_payroll',
+    port: 3306
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
+      return;
+    }
+    console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
+  });
   const { id } = req.query;
   const sql = `
     SELECT payroll.*, empoyee.*, approver.* FROM payroll
@@ -97,9 +160,24 @@ app.get('/api/payrollid', (req, res) => {
     }
     res.json(results);
   });
+  connection.end();
 });
 
 app.get('/api/addpayroll', (req, res) => {
+  const connection = mysql.createConnection({
+    host: '27.254.145.132',
+    user: 'zeniq_admin',
+    password: 'Zeniq12345!',
+    database: 'zeniq_payroll',
+    port: 3306
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
+      return;
+    }
+    console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
+  });
   const { id, month, year, empoyee_id, title, data, title_rm, data_rm, approver } = req.query;
   const sql = "INSERT INTO `payroll`(`id`, `month`, `year`, `empoyee_id`, `title`, `data`, `title_rm`, `data_rm`, `approver`) VALUES (?,?,?,?,?,?,?,?,?)";
 
@@ -109,9 +187,24 @@ app.get('/api/addpayroll', (req, res) => {
     }
     res.json(results);
   });
+  connection.end();
 });
 
 app.get('/api/updatepayroll', (req, res) => {
+  const connection = mysql.createConnection({
+    host: '27.254.145.132',
+    user: 'zeniq_admin',
+    password: 'Zeniq12345!',
+    database: 'zeniq_payroll',
+    port: 3306
+  });
+  connection.connect((err) => {
+    if (err) {
+      console.error('เกิดข้อผิดพลาดในการเชื่อมต่อ:', err.stack);
+      return;
+    }
+    console.log('เชื่อมต่อสำเร็จที่ ID ' + connection.threadId);
+  });
   const { id, month, year, empoyee_id, title, data, title_rm, data_rm, approver } = req.query;
   const sql = "UPDATE `payroll` SET `id`=?,`month`=?,`year`=?,`empoyee_id`=?,`title`=?,`data`=?,`title_rm`=?,`data_rm`=?,`approver`=? WHERE `id` = ?";
 
@@ -121,6 +214,7 @@ app.get('/api/updatepayroll', (req, res) => {
     }
     res.json(results);
   });
+  connection.end();
 });
 
 
